6f501ced39d60370484f56871e97a53f8caefe17
GabrielSBrito17 <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>
2023-03-29 23:42:12 -0300

    ajustes

--- resources/views/layouts/menu-lateral.blade.php
+++ resources/views/layouts/menu-lateral.blade.php
@@ -2,7 +2,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
 <!-- Brand Logo -->
 <a href="../../index3.html" class="brand-link">
-    <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">AdminLTE 3</span>
 </a>
 
@@ -11,22 +10,9 @@
     <!-- Sidebar user (optional) -->
     <div class="user-panel mt-3 pb-3 mb-3 d-flex">
     <div class="image">
-        <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
     </div>
     <div class="info">
         <a href="#" class="d-block">Alexander Pierce</a>
-    </div>
-    </div>
-
-    <!-- SidebarSearch Form -->
-    <div class="form-inline">
-    <div class="input-group" data-widget="sidebar-search">
-        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-        <button class="btn btn-sidebar">
-            <i class="fas fa-search fa-fw"></i>
-        </button>
-        </div>
     </div>
     </div>
 
